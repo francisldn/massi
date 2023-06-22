e36504580add41231293fd6faab89f6dc7a66f5d
Francis_ldn <fnhh04@gmail.com>
2023-06-22 08:17:31 +0100

    edit discount

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -10,9 +10,14 @@ const serviceWidth= 'lg:w-[500px]'
 
 const ServicePage = () => {
     return (
-        <main className="h-[100vh] w-full">
+        <main className="h-[100vh] w-full relative">
         <ServiceHero />
-        <section className="px-[5%] py-auto flex flex-col lg:justify-center service-container lg:mt-0">
+        <div className="-mt-[11rem] lg:-mt-[5rem] mx-auto rounded-full bg-yellow-600 text-white h-40 w-40">
+            <div className="flex items-center h-full w-full justify-center z-10">
+                <p className="text-center text-sm">Receive <br /><span className="text-4xl tracking-widest">10%</span><br/> off your first <br />treatment<br/></p>
+            </div>
+        </div>
+        <section className="px-[5%] mt-[1rem] flex flex-col lg:justify-center service-container lg:mt-[2rem]">
             <div className="lg:h-full lg:items-center lg:justify-center flex flex-col lg:flex-row gap-4">
                 <Service
                     src={relaxing}
@@ -44,11 +49,7 @@ const ServicePage = () => {
 
             </div> 
             </section>
-            <div className="absolute rounded-full bg-yellow-600 text-white top-[20rem] lg:top-[10rem] h-40 w-40 left-[35%] lg:left-[45%]">
-                <div className="flex items-center h-full w-full justify-center">
-                    <p className="text-center text-sm">Receive <br /><span className="text-4xl tracking-widest">10%</span><br/> off your first <br />treatment<br/></p>
-                </div>
-            </div>
+            
         </main>
   )
 }
