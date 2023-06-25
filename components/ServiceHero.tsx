ca133f1404e63228d2057e682568edbfaf1459bb
Francis_ldn <fnhh04@gmail.com>
2023-06-25 17:31:34 +0100

    change cover image

--- components/ServiceHero.tsx
+++ components/ServiceHero.tsx
@@ -8,7 +8,7 @@ const ServiceHero = () => {
           <div className="absolute z-0 text-center w-full h-full sm:text-xl lg:text-2xl text-white pt-[12rem] lg:pt-[4rem]">
               <p className="text-center">At Massi Massage <br /> We help you unwind, de-stress, and uplift <br /> your body and mind. </p>
           </div>
-          <div className='h-[50vh] lg:h-[30vh] w-[100vw] wavy mb-[5rem] lg:mb-[1rem] absolute -z-10'>
+          <div className='h-[50vh] lg:h-[35vh] w-[100vw] wavy mb-[5rem] lg:mb-[1rem] absolute -z-10'>
               <div className="absolute bg-[rgb(0,0,0,0.3)] w-full h-full"></div>
             <Image
                 src={relaxing}
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,5 @@
 export const calendarURL = "https://calendly.com/massimassage";
-export const relaxing = "https://res.cloudinary.com/dac48s3os/image/upload/v1687298655/relaxing_otsi8n.jpg";
+export const relaxing = "https://res.cloudinary.com/dac48s3os/image/upload/v1687298649/remedial_tp1owb.jpg";
 export const deepTissue = "https://res.cloudinary.com/dac48s3os/image/upload/v1687298661/deepTissue_gqryjo.jpg";
 export const remedial = "https://res.cloudinary.com/dac48s3os/image/upload/v1687681128/WhatsApp_Image_2023-06-19_at_21.46.11_lneq5r.jpg"
 export const profile = "https://res.cloudinary.com/dac48s3os/image/upload/v1687210148/profile_dnzybg.jpg"
