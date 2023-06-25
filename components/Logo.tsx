917ce15a8fa23793b73dcd8c420e37f78fff1386
Francis_ldn <fnhh04@gmail.com>
2023-06-25 18:13:09 +0100

    edit logo size

---
+++ components/Logo.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image';
+import { logo } from '@/utils/constants';
+
+const Logo = () => {
+  return (
+    <div className="absolute top-4 left-4 w-[3rem] h-[3rem] lg:w-[5rem] lg:h-[5rem] rounded-full overflow-hidden bg-white">
+          <Image
+            src={ logo}
+            width='0'
+            height='0'
+            sizes='100vw'
+            className='w-full h-full bg-transparent'
+            alt={"picture of a man enjoying a massage"}
+            style={{ objectFit: 'contain' }}
+          />
+        </div>
+  )
+}
+
+export default Logo
