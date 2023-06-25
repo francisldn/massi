5572b3022dfd5c5ab4c36af96cfd0b008148eeb9
Francis_ldn <fnhh04@gmail.com>
2023-06-25 17:39:04 +0100

    remove animation

--- components/ServiceHero.tsx
+++ components/ServiceHero.tsx
@@ -4,7 +4,7 @@ import { serviceHero } from '@/utils/constants';
 
 const ServiceHero = () => {
   return (
-      <section className="relative swing-in-top-fwd -z-10">
+      <section className="relative -z-10">
           <div className="absolute z-0 text-center w-full h-full sm:text-xl lg:text-2xl text-white pt-[12rem] lg:pt-[4rem]">
               <p className="text-center">At Massi Massage <br /> We help you unwind, de-stress, and uplift <br /> your body and mind. </p>
           </div>
